22bd7970b8105e8fb36ea31d0473bff34b33a922
Mathew Thomas <mathew.thomas@longdivision.com>
2018-06-04 21:43:46 -0400

    fixed banner logo
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- src/server.js
+++ src/server.js
@@ -16,19 +16,6 @@ _______ _______ ______  _     _ _______ _______\n\
 |  |  | |______ |_____/ |_____| ______| |     |\n\
 ';
 
-// eTadpoleTadp       ole        adpoleT     eTadpoleTa      adpole    oleTad      dpoleTadpol    \n\
-// leTadpoleTad      dpole       TadpoleTa    eTadpoleTa    eTadpole   poleTa      adpoleTadpol   \n\
-// ole  dpo  Ta      adpol       eTadpoleTa   leTa    eTa  oleT  pole    ole       Tadp    adpo   \n\
-//      adp         eTadpo       leTa   leTa  oleTadpoleT  pol    pol    po         Tad           \n\
-//      Tad         leT dpo       leT   oleT  poleTadpol  adpo    dpol   dp         eTadpo        \n\
-//      eT         poleTadpo      ole    ole  dpoleTadp   Tadp    adpo   ad         leTadpo       \n\
-//      leT        dpoleTadp      pol   dpol  adp          Tad    Tad    Tad        oleTadp       \n\
-//      ole       Tadp   Tadp    adpo  Tadpo  Tad          eTa    eTa   leTa    eT  pol           \n\
-//      pol       eTa     Tad   eTadpoleTadp  eTa          leTa  oleT    leT    le  dpol    pol   \n\
-//    Tadpole   poleTa   leTadp leTadpoleTa  oleTad         leTadpol   dpoleTadpol Tadpole adpo   \n\
-//    eTadpol   dpoleT   oleTad oleTadpol    poleTa          leTadp    adpoleTadpo eTadpoleTadp';
-                                                                                               
-
 console.log(logo);
 
 // App
